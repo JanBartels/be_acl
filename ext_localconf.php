1b252fe58bed78b128ca20e93d587cb124c393fb
j.bartels <j.bartels@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-18 21:06:20 +0000

    Clear be_acl-cache if new pages are inserted.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/be_acl/branches/1.4.1@42326 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,4 +14,6 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['getPagePe
 
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/mod/web/perm/index.php'] = t3lib_extMgm::extPath('be_acl').'res/class.ux_sc_mod_web_perm_index.php';
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:be_acl/class.tx_beacl_hooks.php:tx_beacl_hooks';
+
 ?>
