e2fd9c73fbc2d5aa44d99b18dd4e7d9715a78da6
j.bartels <j.bartels@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-18 21:06:20 +0000

    Clear be_acl-cache if new pages are inserted.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/be_acl@42326 735d13b6-9817-0410-8766-e36946ffe9aa

--- branches/1.4.1/ext_emconf.php
+++ branches/1.4.1/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "be_acl"
+# Extension Manager/Repository config file for ext "be_acl".
 #
-# Auto generated 12-01-2009 16:57
+# Auto generated 18-01-2011 22:03
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Backend Access Control Lists',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '1.4.0',
+	'version' => '1.4.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,8 +34,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
-			'lang' => '',
 			'php' => '3.0.0-0.0.0',
 			'typo3' => '3.5.0-0.0.0',
 		),
@@ -44,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:21:"ext_conf_template.txt";s:4:"23d5";s:12:"ext_icon.gif";s:4:"1bea";s:17:"ext_localconf.php";s:4:"5cdf";s:14:"ext_tables.php";s:4:"aceb";s:14:"ext_tables.sql";s:4:"1076";s:21:"icon_tx_beacl_acl.gif";s:4:"1bea";s:16:"locallang_db.php";s:4:"0f61";s:7:"tca.php";s:4:"01cc";s:14:"doc/manual.sxw";s:4:"4db3";s:19:"doc/wizard_form.dat";s:4:"e68f";s:20:"doc/wizard_form.html";s:4:"4e9b";s:10:"res/acl.js";s:4:"80ec";s:36:"res/class.tx_beacl_userauthgroup.php";s:4:"3d8c";s:38:"res/class.ux_sc_mod_web_perm_index.php";s:4:"4bb6";s:22:"res/locallang_perm.php";s:4:"9a96";}',
+	'_md5_values_when_last_written' => 'a:17:{s:24:"class.tx_beacl_hooks.php";s:4:"9ccb";s:16:"ext_autoload.php";s:4:"001a";s:21:"ext_conf_template.txt";s:4:"23d5";s:12:"ext_icon.gif";s:4:"1bea";s:17:"ext_localconf.php";s:4:"3d80";s:14:"ext_tables.php";s:4:"aceb";s:14:"ext_tables.sql";s:4:"1076";s:21:"icon_tx_beacl_acl.gif";s:4:"1bea";s:16:"locallang_db.php";s:4:"0f61";s:7:"tca.php";s:4:"01cc";s:14:"doc/manual.sxw";s:4:"4db3";s:19:"doc/wizard_form.dat";s:4:"e68f";s:20:"doc/wizard_form.html";s:4:"4e9b";s:10:"res/acl.js";s:4:"80ec";s:36:"res/class.tx_beacl_userauthgroup.php";s:4:"3d8c";s:38:"res/class.ux_sc_mod_web_perm_index.php";s:4:"4bb6";s:22:"res/locallang_perm.php";s:4:"9a96";}',
+	'suggests' => array(
+	),
 );
 
 ?>
