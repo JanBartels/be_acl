49de30c91d37fd42716b21ea814422df024df77a
j.bartels <j.bartels@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-14 19:42:41 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/be_acl@46543 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ branches/1.4.2/branches/1.4.1/ext_localconf.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_beacl_acl=1
+');
+
+
+
+require_once(t3lib_extMgm::extPath('be_acl').'res/class.tx_beacl_userauthgroup.php');
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['calcPerms'][] = 'tx_beacl_userAuthGroup->calcPerms';
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['getPagePermsClause'][] = 'tx_beacl_userAuthGroup->getPagePermsClause';
+
+
+
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/mod/web/perm/index.php'] = t3lib_extMgm::extPath('be_acl').'res/class.ux_sc_mod_web_perm_index.php';
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:be_acl/class.tx_beacl_hooks.php:tx_beacl_hooks';
+
+?>
---
+++ branches/1.4.2/branches/1.4.1/ext_tables.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
+
+t3lib_extMgm::allowTableOnStandardPages("tx_beacl_acl");
+
+$TCA["tx_beacl_acl"] = Array (
+	"ctrl" => Array (
+		"title" => "LLL:EXT:be_acl/locallang_db.php:tx_beacl_acl",
+		"label" => "uid",
+		"tstamp" => "tstamp",
+		"crdate" => "crdate",
+		"cruser_id" => "cruser_id",
+		"type" => "type",
+		"default_sortby" => "ORDER BY type",
+		"dynamicConfigFile" => t3lib_extMgm::extPath($_EXTKEY)."tca.php",
+		"iconfile" => t3lib_extMgm::extRelPath($_EXTKEY)."icon_tx_beacl_acl.gif",
+	),
+	"feInterface" => Array (
+		"fe_admin_fieldList" => "type, object_id, permissions, recursive",
+	)
+);
+
+?>
