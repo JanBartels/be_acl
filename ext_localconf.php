e5a35ecfcbef8191df4335b766ba01d872f0e4b2
lex_frolenko <lex.frolenko.stratis@gmail.com>
2019-05-30 12:19:25 +0300

    [TASK] added enableRedis to ext_conf_template

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$_EXTCONF = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['be_acl'];
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
 	options.saveDocNew.tx_beacl_acl=1
 ');
@@ -19,7 +21,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'JBartels\\BeAcl\\Hook\\DataHandlerHook';
 
 
-$redisLoaded = extension_loaded('redis');
+$redisLoaded = extension_loaded('redis') && $_EXTCONF['enableRedis'];
 
 // set tx_be_acl_timestamp-cache
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_be_acl_timestamp']['frontend'])) {
